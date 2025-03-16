b6881b6e84cdfb210cdc46eb5382a37f0ba5ede6
savad k <124030718+S4vad@users.noreply.github.com>
2025-03-16 15:20:27 +0530

    new

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Blog() {
   return (
@@ -6,22 +7,22 @@ export default function Blog() {
       <div class="flex flex-col gap-3">
         <div class="flex flex-col gap-4">
           <div class="flex hover:opacity-100 text-md flex-col md:flex-row md:items-center justify-between gap-2">
-            <a
+            <Link
               class="text-[#82b817] hover:text-[#a3d42a]  border-b inline-flex items-center gap-1 border-neutral-800 border-dashed  transition-all undefined"
-              href="/blog/1"
+              to="/blog/1"
             >
               The Journey of Building Bizfluence
-            </a>
+            </Link>
             <p class="text-neutral-500">25 Feb, 2025</p>
           </div>
 
           <div class="flex hover:opacity-100 text-md flex-col md:flex-row md:items-center justify-between gap-2">
-            <a
+            <Link
               class="text-[#82b817] hover:text-[#a3d42a]  border-b inline-flex items-center gap-1 border-neutral-800 border-dashed  transition-all undefined"
-              href="/blog/2"
+              to="/blog/2"
             >
               The Habit of Reading: A Journey of Growth and Discovery
-            </a>
+            </Link>
             <p class="text-neutral-500">8 FEB, 2024</p>
           </div>
 
